dd74fb509b6bb8bf16c624052a42318bed74d267
Giggi Argento <giggi.argento@gmail.com>
2022-06-21 14:12:09 -0300

    AddMordernJavaScript

---
+++ Resources/01-Introduccion/js/introduccion.js
@@ -0,0 +1,9 @@
+        //Tipico mensaje para introducir JavaScript
+        alert('Hola Mundo');
+        
+        //Pregunta al usuario su nombre
+        const nombre = prompt('Cual es tu nombre?');
+
+        //Toma el nombre y lo muestra en la pantalla seguido de una frase
+        document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript moderno`;
+    
