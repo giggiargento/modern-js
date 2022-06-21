6e91c7d2efca36858b48f1468bf8356c2eb5ccfb
Giggi Argento <giggi.argento@gmail.com>
2022-06-21 17:07:27 -0300

    Introduction

--- Resources/01-Introduccion/js/introduccion.js
+++ Resources/01-Introduccion/js/introduccion.js
@@ -1,9 +1,34 @@
-        //Tipico mensaje para introducir JavaScript
-        alert('Hola Mundo');
-        
-        //Pregunta al usuario su nombre
-        const nombre = prompt('Cual es tu nombre?');
+//Tipico mensaje para introducir JavaScript
+alert('Hola Mundo');
 
-        //Toma el nombre y lo muestra en la pantalla seguido de una frase
-        document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript moderno`;
-    
+//Pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre?');
+
+//Toma el nombre y lo muestra en la pantalla seguido de una frase
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript moderno`;
+
+
+console.log("test" !== "test2");
+
+//Muestra error en la consola del navegador con la frase que escribamos
+console.error('Algo malio sal');
+
+//Muestra en la consola del navegador cuanto tiempo tardaron en ejecutarse las lineas de codigo que contiene
+console.time('Hola');
+
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+console.warn('WARNING');
+
+console.timeEnd('Hola');
