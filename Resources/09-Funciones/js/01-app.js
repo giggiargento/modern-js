e97ea049cdf15ffc0f76f149c0e8a5ec404763a1
Giggi Argento <giggi.argento@gmail.com>
2022-07-04 15:34:45 -0300

    Starting with functions

---
+++ Resources/09-Funciones/js/01-app.js
@@ -0,0 +1,9 @@
+// Declaracion de funcion (Function Declaration)
+function sumar(){ // Dentro de los parentesis se declara la funcion y dentro de las llaves lo que va a hacer la funcion
+    console.log(2+2);
+}
+
+// Llamar la funcion 
+sumar();
+sumar();
+sumar();
